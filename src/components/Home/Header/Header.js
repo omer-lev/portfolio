16d7352749b40ac0dffb29c91850450ead891e27
omer lev <omerlev66@gmail.com>
2022-01-02 23:46:51 +0200

    home page final touchups*

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <h1>I'm Omer Lev</h1>
+            <h1>I'm <span>Omer</span> Lev</h1>
             <br />
             <h3>Web Developer / Designer</h3>
         </div>
